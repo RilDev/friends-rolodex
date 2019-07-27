10739c1784da98a8aacaa1a82b2ce4e360352bde
CyrilAntoni <cyril.lay.antoni@gmail.com>
2019-07-27 19:21:15 +0200

    add getFriends api call && display friends

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const getFriends = async (url, callback) => {
+  const response = await fetch(url);
+  const friends = await response.json();
+
+  console.log(`friends`, friends);
+  callback(friends);
+};
+
+function App() {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    getFriends(`https://jsonplaceholder.typicode.com/users`, setFriends);
+  }, []);
+
+  return (
+    <div className="App">
+      {friends.length > 0 && friends.map(friend => <h1 key={friend.id}>{friend.name}</h1>)}
+    </div>
+  );
+}
+
+export default App;
