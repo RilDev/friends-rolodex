7ca7d50c76746dd1045450de07c61ba27e430dcc
CyrilAntoni <cyril.lay.antoni@gmail.com>
2019-07-27 20:33:11 +0200

    add card style

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './card.style.css';
 
 const Card = props => (
-	<div>
+	<div class="card-container">
 		<img src={`https://robohash.org/${props.friend.email}?set=set5&size=180x180`} />
-  	<h2>{props.friend.name}</h2>
+	  <h2>{props.friend.name}</h2>
+	  <p>{props.friend.email}</p>
   </div>
 );
 
