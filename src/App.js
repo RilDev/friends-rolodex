aac91729863214d4a1cae0f8404124a53c2d54d0
CyrilAntoni <cyril.lay.antoni@gmail.com>
2019-07-28 19:53:42 +0200

    add card style

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ const getFriends = async (url, callback) => {
   const response = await fetch(url);
   const friends = await response.json();
 
-  console.log(`friends`, friends);
   callback(friends);
 };
 
+const getSearch = (search, friends) => friends.filter(friend => friend.name.toLowerCase().includes(search.toLowerCase()));
+
 function App() {
   const [friends, setFriends] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
 
   useEffect(() => {
     getFriends(`https://jsonplaceholder.typicode.com/users`, setFriends);
   }, []);
 
+  useEffect(() => {
+    setSearchResult(getSearch(search, friends));
+  }, [search, friends]);
+
   return (
     <div className="App">
-      <CardList friends={friends} />
+      <input
+        type="search"
+        placeholder="Search Friend"
+        onChange={e => setSearch(e.target.value)}
+      />
+      <CardList friends={searchResult} />
     </div>
   );
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './card.style.css';
 
 const Card = props => (
-	<div class="card-container">
-		<img src={`https://robohash.org/${props.friend.email}?set=set5&size=180x180`} />
+	<div className="card-container">
+		<img src={`https://robohash.org/${props.friend.email}?set=set5&size=180x180`} alt={`${props.friend.name}`} />
 	  <h2>{props.friend.name}</h2>
 	  <p>{props.friend.email}</p>
   </div>
