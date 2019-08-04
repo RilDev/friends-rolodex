4b87abe6d977cc605bfed7b623288907d1975911
Cyril Antoni <cyril.lay.antoni@gmail.com>
2019-08-04 21:39:00 +0200

    Updates

---
+++ precache-manifest.22827c204e212c9bf05dd75aae29cbf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a89d3dee5c4a2ccebdc5e3e198fbf",
+    "url": "/friends-rolodex/index.html"
+  },
+  {
+    "revision": "8b7e7a1e876a85e83b9e",
+    "url": "/friends-rolodex/static/css/main.b591fb35.chunk.css"
+  },
+  {
+    "revision": "16a6385f42917bcffd60",
+    "url": "/friends-rolodex/static/js/2.71412b09.chunk.js"
+  },
+  {
+    "revision": "8b7e7a1e876a85e83b9e",
+    "url": "/friends-rolodex/static/js/main.16288d81.chunk.js"
+  },
+  {
+    "revision": "b79adc45128b68069078",
+    "url": "/friends-rolodex/static/js/runtime~main.de9bfdd2.js"
+  }
+]);
